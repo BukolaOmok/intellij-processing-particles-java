3eb6cb9b050e4fa388c8e864227ee5e08a3ffc5c
BOmokehinde <BOmokehinde@sis.tv>
2024-08-13 16:08:51 +0100

    triangle works now. Still need to call them at the same time

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,10 +1,25 @@
-//import processing.core.PApplet;
-//
-//public class Triangle extends Spark {
-//    float size;
-//
-//    public Triangle(PApplet p5, int myColour, float size) {
-//        super(p5, myColour);
-//        this.size = size;
-//    }
-//}
+import processing.core.PApplet;
+
+public class Triangle extends Spark {
+    float size;
+
+    public Triangle(PApplet p5, float x, float y) {
+        super(p5, x, y);
+        this.size = p5.random(10, 50);
+    }
+
+    @Override
+    public void display() {
+        p5.fill(myColour);
+        float sizeDividedBy2 = size / 2;
+        p5.triangle(x, y-sizeDividedBy2, x-sizeDividedBy2, y +sizeDividedBy2,x+sizeDividedBy2, y+sizeDividedBy2);
+    }
+
+    @Override
+    public void update() {
+        final int stepSize = 5;
+        x += p5.random(-stepSize, stepSize);
+        y += p5.random(-stepSize, stepSize);
+        size = PApplet.constrain(size + p5.random(-1, 1), 5, 100);
+    }
+}
