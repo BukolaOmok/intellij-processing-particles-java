0ca5c8665c5361a3f8309f5eb449e14e36d6bf8b
BOmokehinde <BOmokehinde@sis.tv>
2024-08-13 15:57:27 +0100

    create base, initiate display and update method to be over ridden, create circle, rectangle and triangle class, still working on making the circle work and updating my spark class

---
+++ src/Rectangle.java
@@ -0,0 +1,12 @@
+import processing.core.PApplet;
+
+public class Rectangle extends Spark {
+    float size;
+
+    public Rectangle(PApplet p5, int myColour, float size) {
+        super(p5, myColour);
+        this.size = size;
+    }
+
+
+}
---
+++ src/Triangle.java
@@ -0,0 +1,10 @@
+//import processing.core.PApplet;
+//
+//public class Triangle extends Spark {
+//    float size;
+//
+//    public Triangle(PApplet p5, int myColour, float size) {
+//        super(p5, myColour);
+//        this.size = size;
+//    }
+//}
